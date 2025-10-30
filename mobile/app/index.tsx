d7e7c0d9032abc397e56ccfcfa27087193670967
Alex Felici <sandroms521@gmail.com>
2025-10-30 11:25:12 -0400

    TextInput + add back pictures

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,10 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Dimensions, DimensionValue, StyleSheet } from "react-native";
+import { TextInput, View } from "react-native";
 
 export default function Index() {
+  const [message, setMessage] = useState<string>("");
+
   return (
     <View
       style={{
@@ -9,7 +13,27 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput
+        value={message}
+        onChangeText={setMessage}
+        style={styles.input}
+        multiline={true}
+      />
     </View>
   );
 }
+
+const screenWidth: DimensionValue = Dimensions.get('screen').width;
+const screenHeight: DimensionValue = Dimensions.get('screen').height;
+
+const marginX: DimensionValue = screenWidth * 0.75;
+const marginY: DimensionValue = screenHeight * 0.6;
+
+const styles = StyleSheet.create({
+  input: {
+    width: marginX,
+    height: marginY,
+    borderWidth: 1,
+    padding: 10,
+  }
+})
