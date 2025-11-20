6d5378937756ca58a8e65352fc00c334b3406c82
Alex Felici <sandroms521@gmail.com>
2025-11-20 10:58:15 -0500

    Display error

--- desktop/src/renderer/views/login.js
+++ desktop/src/renderer/views/login.js
@@ -36,17 +36,22 @@ export const renderLoginView = (onLoginSuccess) => {
   /*
    *  Setup new account or login new account on respective button press
    */
-  const registerAccount = async (firebaseFunction) => {
+  const registerAccount = async (type) => {
+    const firebaseFunction = type == "signIn" ? contactFirebase.signIn : contactFirebase.signUp
     const email = emailInput.value;
     const password = passwordInput.value;
     const res = await firebaseFunction(email, password);
     if (res) {
       onLoginSuccess();
     }
+    else {
+      const signInError = "Invalid Credentials";
+      const signUpError = "Email or password invalid. Password must contain 6 characters."
+      showError.innerHTML = type == "signIn" ? signInError : signUpError;
+    }
   }
 
-  signInButton.addEventListener('click', () => registerAccount(contactFirebase.signIn))
-  signUpButton.addEventListener('click', () => registerAccount(contactFirebase.signUp))
-
+  signInButton.addEventListener('click', () => registerAccount("signIn"))
+  signUpButton.addEventListener('click', () => registerAccount("signUp"))
   document.body.appendChild(loginPage);
 }
